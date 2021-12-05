5c66cfc892584bbca39cd31154bdac19aa285bc8
Bryan <bryanmora1994@gmail.com>
2021-12-04 23:00:20 -0600

    homesa

--- view/home.js
+++ view/home.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.route('/').get((req, res)=>{
-    res = () => '<center><h1>This is home</h1></center>';
+    res.send('<center><h1>This is home</h1></center>');
 });
 
 module.exports = router;
